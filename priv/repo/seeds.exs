5f89aa0f52fb0b57b56a142858ed41019f991cad
Ascani Carlo <carlotm@pm.me>
2022-10-08 18:53:08 +0200

    Load seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,21 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Msw.Repo.insert!(%Msw.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+defmodule Msw.Seeds do
+  @root_dir File.cwd!()
+  @data_dir Path.join(@root_dir, "priv/data")
+  @seeds [
+    {"seasons", "id, number, external_id, inserted_at, updated_at"},
+    {"episodes",
+     "id, number, external_id, title, plot, poster, season_id, inserted_at, updated_at"},
+    {"killers", "id, name, episode_id, picture, picture64, inserted_at, updated_at"}
+  ]
+
+  defp load({table, columns}) do
+    file = Path.join(@data_dir, "#{table}.csv")
+    Msw.Repo.query!("COPY #{table} (#{columns}) FROM '#{file}' WITH (FORMAT csv, HEADER True)")
+  end
+
+  def run() do
+    Enum.each(@seeds, &load/1)
+  end
+end
+
+Msw.Seeds.run()
